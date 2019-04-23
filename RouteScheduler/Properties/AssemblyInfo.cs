289257a6b0736029a2e1e6011ee0476341604e2b
samsmllr <sam.s.mllr@gmail.com>
2019-04-23 10:31:15 -0500

    Recreated the initial project to include individual user authentication.

--- RouteScheduler/Properties/AssemblyInfo.cs
+++ RouteScheduler/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b2b0fec8-55da-4f2a-9da2-d91b052f0e69")]
+[assembly: Guid("0cedcc58-e3af-41f0-acc2-3f0d461419af")]
 
 // Version information for an assembly consists of the following four values:
 //
