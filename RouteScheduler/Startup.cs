459aebe2136053eb6881bed8b6c6b816230b163d
samsmllr <sam.s.mllr@gmail.com>
2019-04-25 08:09:07 -0500

    created roles. All modifications made are for the creation of roles.

--- RouteScheduler/Startup.cs
+++ RouteScheduler/Startup.cs
@@ -1,7 +1,11 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using RouteScheduler.Models;
 
 [assembly: OwinStartupAttribute(typeof(RouteScheduler.Startup))]
+
 namespace RouteScheduler
 {
     public partial class Startup
@@ -9,6 +13,25 @@ namespace RouteScheduler
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRoles();
+        }
+        
+        private void createRoles()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            if (!roleManager.RoleExists("BusinessOwner"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "BusinessOwner";
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists("Customer"))
+            {
+                var role = new Microsoft.AspNet.Identity.EntityFramework.IdentityRole();
+                role.Name = "Customer";
+                roleManager.Create(role);
+            }
         }
     }
 }
